0b17835552eb87daea28b5f85dafe847418d80b6
AlexeyZip <alexzipzap@gmail.com>
2023-04-11 18:25:55 +0300

    mean: added post-list

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+ public posts = [
+    {
+        title: 'Post1',
+        content: 'Test content 1'
+    },
+    {
+        title: 'Post2',
+        content: 'Test content 2'
+    },
+    {
+        title: 'Post3',
+        content: 'Test content 3'
+    },
+ ]
+}
