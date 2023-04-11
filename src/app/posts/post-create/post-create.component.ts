1695f74a989876b9681501bca7da0568c95633af
AlexeyZip <alexzipzap@gmail.com>
2023-04-11 15:26:54 +0300

    mean: added material

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,8 +2,13 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-
+    public newPost: string = 'NO CONTENT';
+    public enteredValue: string = '';
+  public onAddPost(): void {
+    this.newPost = this.enteredValue;
+  }
 }
