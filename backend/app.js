ff775b4e9544f51643a8423e21c1d3c055ae0736
AlexeyZip <alexzipzap@gmail.com>
2023-04-24 22:44:03 +0300

    mean: set up back-end

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,54 @@
 const express = require('express');
+const bodyParser = require("body-parser");
+
+
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
-    console.log('first middleware');
-    next();    
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS"); // Options is an implict request sent by the browser
+    next();
+})
+
+app.post("/api/posts", (req, res, next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).json({
+        message: 'Post added successfully'
+    });
 });
 
-app.use((req, res) => {
-    res.send('express!');
+app.get('/api/posts',(req, res) => {
+    const posts = [
+        {
+            id: '1',
+            title: 'Title 1',
+            content: 'Post number 1'
+        },
+        {
+            id: '2',
+            title: 'Title 2',
+            content: 'Post number 2'
+        },
+        {
+            id: '3',
+            title: 'Title 3',
+            content: 'Post number 3'
+        },
+        {
+            id: '4',
+            title: 'Title 4',
+            content: 'Post number 4'
+        },
+    ]
+    res.status(200).json({
+        message: 'Posts fetched succesfully!',
+        posts: posts
+    });
 });
 
 
