ee38ebd762adda0a988459a5e20e9bbe29909de4
AlexeyZip <alexzipzap@gmail.com>
2023-05-04 10:45:33 +0300

    mean: added implementation of edit post

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS"); // Options is an implict request sent by the browser
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS"); // Options is an implict request sent by the browser
     next();
 })
 
@@ -34,6 +34,20 @@ app.post("/api/posts", (req, res, next) => {
             message: 'Post added successfully',
             postId: createdPost._id
         });
+    });
+});
+
+app.put("/api/posts/:id", (req, res, next) => {
+    const updatedPost = new Post({
+        _id: req.body.id,
+        title: req.body.title,
+        content: req.body.content
+    })
+    Post.updateOne({_id: req.params.id}, updatedPost).then(result => {
+        console.log(result);
+        res.status(200).json({
+            message: "Update successful!"
+        })
     });
 });
 
---
+++ src/app/posts/post.interface.ts
@@ -0,0 +1,4 @@
+export enum postMode {
+    editMode = 'edit',
+    createMode = 'create'
+}
